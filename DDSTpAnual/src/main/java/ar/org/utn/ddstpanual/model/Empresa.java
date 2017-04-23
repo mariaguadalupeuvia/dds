e108e4127e7da3cff34c0f7f744a966a1fbe8737
miguel-selaez <migue.sel@gmail.com>
2017-04-23 19:33:05 -0300

    Se agrego el atributo "nombre" a la clase Empresa

--- DDSTpAnual/src/main/java/ar/org/utn/ddstpanual/model/Empresa.java
+++ DDSTpAnual/src/main/java/ar/org/utn/ddstpanual/model/Empresa.java
@@ -5,13 +5,22 @@ import java.util.List;
 public class Empresa {
 
   List<Cuenta> cuentas;
-
+  String nombre;
+  
   public List<Cuenta> getCuentas() {
     return cuentas;
   }
 
   public void setCuentas(List<Cuenta> cuentas) {
     this.cuentas = cuentas;
+  }
+  
+  public String getNombre(){
+	  return nombre;
+  }
+  
+  public void setNombre(String nombre){
+	  this.nombre = nombre;
   }
 
 }
