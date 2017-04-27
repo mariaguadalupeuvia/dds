295fd6d76231235bc254a20af5042a9bc1a1cae2
miguel-selaez <migue.sel@gmail.com>
2017-04-27 01:43:08 -0300

    Pruebas unitarias (Junit): Realizar un test automatizado realizando un caso en que el usuario ingrese un arhcivod e carga (Excel) al sistema.

---
+++ DDSTpAnual/src/test/java/DDSTpAnual/EmpresaServiceImplTest.java
@@ -0,0 +1,46 @@
+package DDSTpAnual;
+
+import org.junit.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import ar.org.utn.ddstpanual.exception.ServiceException;
+import ar.org.utn.ddstpanual.service.impl.EmpresaServiceImpl;
+
+public class EmpresaServiceImplTest {
+	File file = null;
+	FileInputStream fileStream = null;
+	EmpresaServiceImpl service = null;
+	
+	@Before
+	public void init() throws FileNotFoundException {
+		file = new File("C:/Users/migue/workspace/git/2017-vn-group-24/DDSTpAnual/src/main/resources/Carga1.xlsx");
+		fileStream = new FileInputStream(file);
+		service = new EmpresaServiceImpl();
+	}
+
+	@Test 
+	public void testSubirExcel() {
+		try {
+			service.subirExcel(fileStream);
+		} catch (ServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@After
+	public void finish (){
+	
+		try {
+			if (fileStream != null)
+				fileStream.close();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		
+	}
+
+}
