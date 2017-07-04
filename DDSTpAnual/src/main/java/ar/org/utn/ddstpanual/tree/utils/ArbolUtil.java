bd33c83da2de824223528e3fc3467547d47f78b2
ngomez <ngomez@WKS129.softoffice.com.ar>
2017-07-04 14:16:06 -0300

    Corrección de convertFormulaToArbol. El atributo root del Arbol que retorna ya no apunta a Null.
    Se agregó método obtenerValor.
    Parámetros: Formula, Período, Empresa
    Retorna: Double

--- DDSTpAnual/src/main/java/ar/org/utn/ddstpanual/tree/utils/ArbolUtil.java
+++ DDSTpAnual/src/main/java/ar/org/utn/ddstpanual/tree/utils/ArbolUtil.java
@@ -1,9 +1,12 @@
 package ar.org.utn.ddstpanual.tree.utils;
 
-import java.util.Scanner;
+//import java.util.Scanner;
 import java.util.Stack;
 
 import ar.org.utn.ddstpanual.exception.ServiceException;
+import ar.org.utn.ddstpanual.model.Empresa;
+//import ar.org.utn.ddstpanual.model.FormulaIndicador;
+import ar.org.utn.ddstpanual.model.Periodo;
 import ar.org.utn.ddstpanual.tree.model.Arbol;
 import ar.org.utn.ddstpanual.tree.model.Node;
 import ar.org.utn.ddstpanual.tree.model.OperadorNode;
@@ -14,13 +17,16 @@ public class ArbolUtil {
     Arbol arbol = new Arbol();
     arbol.setFormula(formula);
     Stack<Node> formulaPost = FormulaUtil.inToPost(formula);
+    
+    while(formulaPost.peek() == null){formulaPost.pop();}
+    
     Node root = formulaPost.pop();
-
+    
     if (root.getTypeNode().equals(Node.OPERADOR)) {
       ((OperadorNode) root).setRigthNode(obtenerSiguienteNodo(formulaPost));
       ((OperadorNode) root).setLeftNode(obtenerSiguienteNodo(formulaPost));
     }
-
+    arbol.setRoot(root);
     return arbol;
   }
 
@@ -35,13 +41,27 @@ public class ArbolUtil {
     return nodo;
   }
 
+  public double obtenerValor(String formula, Periodo periodo, Empresa empresa){
+	double valor = 0;
+	try {
+		Arbol arbol = convertFormulaToArbol(formula);
+		valor = arbol.getRoot().obtenerValor(periodo, empresa);
+	} catch (ServiceException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	  catch(NullPointerException e){
+		  e.printStackTrace();
+	  }
+	return valor;
+	  
+  }
 
   public static void main(String[] args) {
     // Entrada de datos
-    System.out.println("*Escribe una expresión algebraica: ");
-    Scanner leer = new Scanner(System.in);
     try {
-      Stack<Node> formulaPost = FormulaUtil.inToPost(leer.nextLine());
+    	Arbol arbol = convertFormulaToArbol("[c1]+[c2]/(([c3]*[c4])+50)");
+    	System.out.println(arbol.getRoot().getTypeNode());
     } catch (ServiceException e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
