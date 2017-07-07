ca606f4ea672b160496c4c9e45f35e3496d24e16
ngomez <ngomez@WKS129.softoffice.com.ar>
2017-07-07 15:24:21 -0300

    Se agregó manejo de errores

--- DDSTpAnual/src/main/java/ar/org/utn/ddstpanual/tree/utils/ArbolUtil.java
+++ DDSTpAnual/src/main/java/ar/org/utn/ddstpanual/tree/utils/ArbolUtil.java
@@ -3,6 +3,10 @@ package ar.org.utn.ddstpanual.tree.utils;
 //import java.util.Scanner;
 import java.util.Stack;
 
+import ar.org.utn.ddstpanual.exception.ArbolException;
+import ar.org.utn.ddstpanual.exception.FormulaInfinitaException;
+import ar.org.utn.ddstpanual.exception.NoEncuentraFormulaException;
+import ar.org.utn.ddstpanual.exception.NoSeEncuentraCuentaException;
 import ar.org.utn.ddstpanual.exception.ServiceException;
 import ar.org.utn.ddstpanual.model.Empresa;
 //import ar.org.utn.ddstpanual.model.FormulaIndicador;
@@ -13,60 +17,49 @@ import ar.org.utn.ddstpanual.tree.model.OperadorNode;
 
 public class ArbolUtil {
 
-  public static Arbol convertFormulaToArbol(String formula) throws ServiceException {
-    Arbol arbol = new Arbol();
-    arbol.setFormula(formula);
-    Stack<Node> formulaPost = FormulaUtil.inToPost(formula);
-    
-    while(formulaPost.peek() == null){formulaPost.pop();}
-    
-    Node root = formulaPost.pop();
-    
-    if (root.getTypeNode().equals(Node.OPERADOR)) {
-      ((OperadorNode) root).setRigthNode(obtenerSiguienteNodo(formulaPost));
-      ((OperadorNode) root).setLeftNode(obtenerSiguienteNodo(formulaPost));
-    }
-    arbol.setRoot(root);
-    return arbol;
-  }
+	public static Arbol convertFormulaToArbol(String formula) throws ServiceException {
+		Arbol arbol = new Arbol();
+		arbol.setFormula(formula);
+		Stack<Node> formulaPost = FormulaUtil.inToPost(formula);
 
-  public static Node obtenerSiguienteNodo(Stack<Node> formulaPost) {
-    Node nodo = formulaPost.pop();
-    if (nodo.getTypeNode().equals(Node.OPERADOR)) {
-      ((OperadorNode) nodo).setRigthNode(obtenerSiguienteNodo(formulaPost));
-      ((OperadorNode) nodo).setLeftNode(obtenerSiguienteNodo(formulaPost));
-    } else {
-      return nodo;
-    }
-    return nodo;
-  }
+		while (formulaPost.peek() == null) {
+			formulaPost.pop();
+		}
 
-  public double obtenerValor(String formula, Periodo periodo, Empresa empresa){
-	double valor = 0;
-	try {
-		Arbol arbol = convertFormulaToArbol(formula);
-		valor = arbol.getRoot().obtenerValor(periodo, empresa);
-	} catch (ServiceException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+		Node root = formulaPost.pop();
+
+		if (root.getTypeNode().equals(Node.OPERADOR)) {
+			((OperadorNode) root).setRigthNode(obtenerSiguienteNodo(formulaPost));
+			((OperadorNode) root).setLeftNode(obtenerSiguienteNodo(formulaPost));
+		}
+		arbol.setRoot(root);
+		return arbol;
 	}
-	  catch(NullPointerException e){
-		  e.printStackTrace();
-	  }
-	return valor;
-	  
-  }
 
-  public static void main(String[] args) {
-    // Entrada de datos
-    try {
-    	Arbol arbol = convertFormulaToArbol("[c1]+[c2]/(([c3]*[c4])+50)");
-    	System.out.println(arbol.getRoot().getTypeNode());
-    } catch (ServiceException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
+	public static Node obtenerSiguienteNodo(Stack<Node> formulaPost) {
+		Node nodo = formulaPost.pop();
+		if (nodo.getTypeNode().equals(Node.OPERADOR)) {
+			((OperadorNode) nodo).setRigthNode(obtenerSiguienteNodo(formulaPost));
+			((OperadorNode) nodo).setLeftNode(obtenerSiguienteNodo(formulaPost));
+		} else {
+			return nodo;
+		}
+		return nodo;
+	}
 
-  }
+	public double obtenerValor(String formula, Periodo periodo, Empresa empresa)
+			throws ServiceException, ArbolException, NoEncuentraFormulaException, NoSeEncuentraCuentaException, FormulaInfinitaException {
+		double valor = 0;
+		try {
+			Arbol arbol = convertFormulaToArbol(formula);
+			valor = arbol.getRoot().obtenerValor(periodo, empresa);
+		} catch (NullPointerException e) {
+			throw new NullPointerException(e.getMessage());
+		} catch (StackOverflowError e){
+			throw new StackOverflowError(e.getMessage());
+		}
+		
+		return valor;
 
+	}
 }
