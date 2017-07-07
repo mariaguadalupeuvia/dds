f7e71be8d4b6e460bfb1e6bb4f86f2b486ead70a
ngomez <ngomez@WKS129.softoffice.com.ar>
2017-07-07 15:34:03 -0300

    Se agregó método "verificarAnalisisFormula"

--- DDSTpAnual/src/main/java/ar/org/utn/ddstpanual/tree/utils/IndicadorUtil.java
+++ DDSTpAnual/src/main/java/ar/org/utn/ddstpanual/tree/utils/IndicadorUtil.java
@@ -1,21 +1,30 @@
 package ar.org.utn.ddstpanual.tree.utils;
 
+import ar.org.utn.ddstpanual.exception.FormulaInfinitaException;
 import ar.org.utn.ddstpanual.tree.model.IndicadorNode;
 
 public class IndicadorUtil {
-  
-  public static void guardarIndicador(IndicadorNode indicador){
-    
-  }
-  
-  public static IndicadorNode obtenerIndicador(String nombreIndicador){
-    
-    return new IndicadorNode(nombreIndicador);
-  }
-  
-  public static boolean existeIndicador(String nombreIndicador){
-    return true;
-  }
-  
-}
+	private static int contador = 0;
+	
+	public static void guardarIndicador(IndicadorNode indicador) {
 
+	}
+
+	public static IndicadorNode obtenerIndicador(String nombreIndicador) {
+
+		return new IndicadorNode(nombreIndicador);
+	}
+
+	public static boolean existeIndicador(String nombreIndicador) {
+		return true;
+	}
+
+	public void verificarAnalisisFormula() throws FormulaInfinitaException{
+	  contador++;
+	  if(contador == 10){
+		  contador = 0;
+		  throw new FormulaInfinitaException("No puede terminar de evaluarse el indicador. Verifique la formula");
+	  }
+  }
+
+}
