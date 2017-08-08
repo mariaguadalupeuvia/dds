23306ea6a3c125e91730919f9aa8d7e06792e59e
ngomez <ngomez@WKS120.softoffice.com.ar>
2017-08-08 17:27:38 -0300

    Se agrega parámetro "Periodo" al método cumpleCondicion

--- DDSTpAnual/src/main/java/ar/org/utn/ddstpanual/service/impl/CondicionServiceImpl.java
+++ DDSTpAnual/src/main/java/ar/org/utn/ddstpanual/service/impl/CondicionServiceImpl.java
@@ -3,6 +3,7 @@ package ar.org.utn.ddstpanual.service.impl;
 import ar.org.utn.ddstpanual.exception.CondicionException;
 import ar.org.utn.ddstpanual.exception.ServiceException;
 import ar.org.utn.ddstpanual.model.Empresa;
+import ar.org.utn.ddstpanual.model.Periodo;
 import ar.org.utn.ddstpanual.model.metodologia.Condicion;
 import ar.org.utn.ddstpanual.model.metodologia.Metodologia;
 import ar.org.utn.ddstpanual.service.CondicionService;
@@ -10,11 +11,11 @@ import ar.org.utn.ddstpanual.service.CondicionService;
 public class CondicionServiceImpl implements CondicionService {
 
   @Override
-  public boolean cumpleCondiciones(Metodologia metodologia, Empresa empresa) throws ServiceException {
+  public boolean cumpleCondiciones(Metodologia metodologia, Empresa empresa, Periodo periodo) throws ServiceException {
     try {
       boolean cumpleCond = true;
       for (Condicion condicion : metodologia.getCondiciones()) {
-        if (!condicion.cumpleCondicion(empresa)) {
+        if (!condicion.cumpleCondicion(empresa, periodo)) {
           cumpleCond = false;
         }
       }
