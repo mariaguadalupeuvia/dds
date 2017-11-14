b390a0458cc984e01c195e98f37a6f44d516cffb
iwolfsdorf <wolfsdorf.ivan@gmail.com>
2017-11-14 17:28:37 -0300

    -Se elimina import sin uso.

--- DDSTpAnual/src/test/java/testEntidades/EmpresaTest.java
+++ DDSTpAnual/src/test/java/testEntidades/EmpresaTest.java
@@ -1,21 +1,19 @@
 package testEntidades;
 
-import static org.junit.Assert.*;
-
 import org.junit.Before;
 import org.junit.Test;
 
 
 public class EmpresaTest {
-  
+
   @Before
   public void init() {
-    
+
   }
-  
+
   @Test
   public void testPeriodoConFecha2013() {
-    //assertEquals("2013", fixture.periodo2013.getFecha());
+    // assertEquals("2013", fixture.periodo2013.getFecha());
   }
-  
+
 }
