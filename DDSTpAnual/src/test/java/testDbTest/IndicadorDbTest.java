e7c6a541a23e61de1e76445e1daa4230d22ed8ae
nicolas-m-gomez <gomez.nicolas.martin@gmail.com>
2017-11-16 22:32:59 -0300

    Se agregan test de indicadorDb

--- DDSTpAnual/src/test/java/testDbTest/IndicadorDbTest.java
+++ DDSTpAnual/src/test/java/testDbTest/IndicadorDbTest.java
@@ -1,85 +1,100 @@
 package testDbTest;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-
-import org.junit.Before;
-import org.junit.Test;
+import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
 
-import ar.org.utn.ddstpanual.db.IndicadorDb;
-import ar.org.utn.ddstpanual.db.UsuarioDb;
+import org.junit.Before;
+import org.junit.Test;
+import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
+
 import ar.org.utn.ddstpanual.exception.DbException;
 import ar.org.utn.ddstpanual.model.Indicador;
-import ar.org.utn.ddstpanual.model.Usuario;
+import fixture.FixtureIndicador;
 
-public class IndicadorDbTest {
+public class IndicadorDbTest implements WithGlobalEntityManager {
+	private FixtureIndicador fixture;
 
-  IndicadorDb indicadorDb = null;
-  Indicador indicador = null;
-  Indicador indicadorBusqueda1;
-  Indicador indicadorBusqueda2;
-  Indicador indicadorPrueba;
-  Usuario usuario;
+	@Before
+	public void iniciarValores() throws DbException {
+		fixture = new FixtureIndicador();
+		fixture.cargarIndicadores();
+	}
 
-  @Before
-  public void init() throws DbException {
-    indicadorDb = new IndicadorDb();
-    UsuarioDb usuarioDb = new UsuarioDb();
-    usuario = usuarioDb.obtenerUsuario("ivan");
+	// Test guardar indicador
+	@Test
+	public void testGuardar() {
+		try {
+			entityManager().getTransaction().begin();
+			fixture.indicadorDb.guardarIndicador(fixture.indTest1);
+		} catch (DbException e) {
+			fail("Error al ejecutar el test");
+		} finally {
+			entityManager().getTransaction().rollback();
+		}
 
-    indicadorBusqueda1 = new Indicador("IndicadorBusqueda1", "[pasivoNoCorriente]*2", usuario.getId());
-    indicadorBusqueda2 = new Indicador("IndicadorBusqueda2", "[pasivoNoCorriente]*{IndicadorBusqueda1}", usuario.getId());
-  }
+	}
 
-  // Test guardar indicador
-  @Test
-  public void testGuardar() throws DbException {
-    indicador = new Indicador("IndicadorDb", "(2*[pasivoCorriente])", usuario.getId());
-    indicadorDb.guardarIndicador(indicador);
-  }
+	// Test sobre la existencia de datos
+	@Test
+	public void testExisteIndicador() {
+		try {
+			fixture.indicadorDb.guardarIndicador(fixture.indTest2);
+			assertTrue(fixture.indicadorDb.exists(fixture.indTest2));
+		} catch (DbException e) {
+			fail("Error al ejecutar el test");
+		} finally {
+			entityManager().getTransaction().rollback();
+		}
+	}
 
-  // Test sobre la existencia de datos
-  @Test
-  public void testExisteIndicador() throws DbException {
-    indicador = new Indicador("ROE", "([beneficioNeto]/[patrimonioNeto])*100", usuario.getId());
-    indicadorDb.guardarIndicador(indicador);
-    assertTrue(indicadorDb.exists(indicador));
-  }
+	@Test
+	public void testNoExisteIndicador() {
+		try {
+			assertFalse(fixture.indicadorDb.exists(fixture.indTest3));
+		} catch (DbException e) {
+			fail("Error al ejecutar el test");
+		} finally {
+			entityManager().getTransaction().rollback();
+		}
+	}
 
-  @Test
-  public void testNoExisteIndicador() throws DbException {
-    indicador = new Indicador("IndicadorArchivoNoExistente", "[CuentaB]+200", usuario.getId());
-    assertFalse(indicadorDb.exists(indicador));
-  }
+	// Test sobre los datos guardados
+	@Test
+	public void testObtenerIndicadorDeUsuario() {
+		try {
+			if (!fixture.indicadorDb.exists(fixture.indTest1)) {
+				fixture.indicadorDb.guardarIndicador(fixture.indTest1);
+			}
 
-  // Test sobre los datos guardados
-  @Test
-  public void testObtenerIndicadorDeUsuario() throws DbException {
-    if (!indicadorDb.exists(indicadorBusqueda1)) {
-      indicadorDb.guardarIndicador(indicadorBusqueda1);
-    }
+			if (!fixture.indicadorDb.exists(fixture.indTest2)) {
+				fixture.indicadorDb.guardarIndicador(fixture.indTest2);
+			}
 
-    if (!indicadorDb.exists(indicadorBusqueda2)) {
-      indicadorDb.guardarIndicador(indicadorBusqueda2);
-    }
+			ArrayList<Indicador> lista = new ArrayList<Indicador>();
+			lista.add(fixture.indTest1);
+			lista.add(fixture.indTest2);
 
-    ArrayList<Indicador> lista = new ArrayList<Indicador>();
-    lista.add(indicadorBusqueda1);
-    lista.add(indicadorBusqueda2);
+			assertTrue(fixture.indicadorDb.obtenerIndicadoresPorUsuario(fixture.usuario.getId()).containsAll(lista));
+		} catch (DbException e) {
+			fail("Error al ejecutar el test");
+		} finally {
+			entityManager().getTransaction().rollback();
+		}
+	}
 
-    assertTrue(indicadorDb.obtenerIndicadoresPorUsuario(usuario.getId()).containsAll(lista));
-  }
-
-  // @Test
-  // public void testObtenerFormulaNoExistente() throws DbException {
-  // assertEquals("", indicadorDb.obtenerFormula("IndicadorInexistente"));
-  // }
-  //
-  // @Test
-  // public void testObtenerNombreIndicador() throws DbException {
-  // assertEquals("ROE", indicadorDb.obtenerNombre("([beneficioNeto]/[patrimonioNeto])*100"));
-  // }
-
+	@Test
+	public void testObtenerFormulaNoExistente() {
+		try {
+			assertEquals("([CuentaPrb1]+[CuentaPrb2])*[CuentaPrb]",
+					fixture.indicadorDb.obtenerIndicador(fixture.indTest4.getNombre()).getFormula());
+		} catch (DbException e) {
+			fail("Error al ejecutar el test");
+		} finally {
+			entityManager().getTransaction().rollback();
+		}
+	}
 }
